49550595c1202cae06874919fe7b666c1ffd0936
danisanz <daniDNZ@users.noreply.github.com>
2022-10-23 19:39:06 +0200

    RequireAuth Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import RequireAuth from './components/RequireAuth';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/login' element={<></>} />
-        <Route path='/' element={<></>} />
+        <Route path='/' element={(
+          <RequireAuth>
+            <Home />
+          </RequireAuth>
+        )} />
+        <Route path='login' element={<Login />} />
       </Routes>
     </div>
   );
