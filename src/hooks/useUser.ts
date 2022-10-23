0b909f02f3ff75e6695340e5604a47c8e2b76745
danisanz <daniDNZ@users.noreply.github.com>
2022-10-23 19:38:36 +0200

    login & logout added

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,33 +1,53 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-export interface userState {
+export interface IUserState {
   logged: boolean;
   values: {
     username: string;
-    token: string;
+    email: string;
   }
 }
 
 export default function useUser() {
-  const initialState: userState = {
-    logged: false,
-    values: {
-      username: '',
-      token: ''
+  const loggedUserJSON = localStorage.getItem('postManagerUserData');
+  const initialState: IUserState = loggedUserJSON 
+    ? JSON.parse(loggedUserJSON)
+    : {
+      logged: false,
+      values: {
+        username: '',
+        email: ''
+      }
     }
-  }
+
   const [user, setUser] = useState(initialState);
 
-  useEffect(() => {
-    const loggedUserJSON = localStorage.getItem('postManagerUserData');
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON);
-      setUser(user);
+  const loginUser = ({ username, email }: { username: string; email: string }) => {
+    const loggedUser: IUserState = {
+      logged: true,
+      values: {
+        username,
+        email
+      }
     }
+    localStorage.setItem('postManagerUserData', JSON.stringify(loggedUser));
+    setUser(loggedUser);
+  }
 
-  }, [])
+  const logoutUser = () => {
+    localStorage.removeItem('postManagerUserData');
+    setUser({
+      logged: false,
+      values: {
+        username: '',
+        email: ''
+      }
+    })
+  }
 
   return {
-    user
+    user,
+    loginUser,
+    logoutUser
   }
 }
