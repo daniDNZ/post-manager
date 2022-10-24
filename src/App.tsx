e4a4d187eeea1ebdae8cc4cfcd3a4d3ed22b9d49
danisanz <daniDNZ@users.noreply.github.com>
2022-10-24 12:07:12 +0200

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import RequireAuth from './components/RequireAuth';
 import Home from './pages/Home';
+import Layout from './pages/Layout';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={(
-          <RequireAuth>
-            <Home />
-          </RequireAuth>
-        )} />
-        <Route path='login' element={<Login />} />
-      </Routes>
-    </div>
+    <Layout>
+      <main className='main'>
+          <Routes>
+            <Route path='/' element={(
+              <RequireAuth>
+                <Home />
+              </RequireAuth>
+            )} />
+            <Route path='login' element={<Login />} />
+          </Routes>
+      </main>
+    </Layout>
   );
 }
 
