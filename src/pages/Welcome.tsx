a304253b3adae84c1c91c6f9455458e74afdfc12
danisanz <daniDNZ@users.noreply.github.com>
2022-10-29 17:38:27 +0200

    Welcome page

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,12 @@
+export default function Welcome() {
+  return (
+    <div className="login">
+      <div className="login__content">
+        <span className="login__welcome">WELCOME!</span>
+        <div className="login__logo-container">
+          <span className="login__logo">Post Manager</span>
+        </div>
+      </div>
+    </div>
+  );
+}
