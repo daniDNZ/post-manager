3d7839a201ae34b3bb4ebd3ec188f2fc981c411f
danisanz <daniDNZ@users.noreply.github.com>
2022-10-29 17:37:11 +0200

    Layout simplified

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,19 +1,15 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
 type Props = {
   children: JSX.Element;
-}
+};
 export default function Layout({ children }: Props) {
   return (
     <>
-      <header className="header">
-        <img className="header__logo" src="./logo.svg" alt="PostManager" loading="lazy"></img>
-        <button className="header__user-button"></button>
-      </header>
-      { children }
-      <footer className="footer">
-        <span className="footer__text">
-          Made with 🤍 by Dani Sanz
-        </span>
-      </footer>
+      <Header />
+      {children}
+      <Footer />
     </>
-  )
+  );
 }
