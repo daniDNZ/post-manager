7af810d4c4c44c4552594f2dad0ff90e4ece3b1c
danisanz <daniDNZ@users.noreply.github.com>
2022-10-29 17:33:07 +0200

    New Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import RequireAuth from './components/RequireAuth';
-import Home from './pages/Home';
-import Layout from './pages/Layout';
-import Login from './pages/Login';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import RequireAuth from "./components/RequireAuth";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <Layout>
-      <main className='main'>
-          <Routes>
-            <Route path='/' element={(
+      <main className="main">
+        <Routes>
+          <Route
+            path="/"
+            element={
               <RequireAuth>
                 <Home />
               </RequireAuth>
-            )} />
-            <Route path='login' element={<Login />} />
-          </Routes>
+            }
+          />
+          <Route path="login" element={<Welcome />} />
+        </Routes>
       </main>
     </Layout>
   );
