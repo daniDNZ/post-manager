c7acc918e599b5def9cd455fd045ad371041271f
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 19:18:09 +0100

    offcanvas reducer added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import offcanvasReducer from '../features/offcanvas/offcanvasSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    offcanvas: offcanvasReducer,
   },
 });
 
