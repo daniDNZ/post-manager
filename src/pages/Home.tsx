f3d5bdafc04fdeaffe16966a20bc94d11aca4a93
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 16:21:14 +0100

    FilterButton and Offcanvas added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,20 @@
 import Card from "../components/Card";
+import FilterButton from "../components/FilterButton";
+import Offcanvas from "../components/Offcanvas";
 
 export default function Home() {
   const cardSamples = [
     {
       title: "Esto es una prueba",
       userID: 2,
+      postID: 1,
       content:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
     },
     {
       title: "Esto es una prueba 2",
       userID: 3,
+      postID: 2,
       content:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
     },
@@ -18,8 +22,10 @@ export default function Home() {
   return (
     <div className="home">
       {cardSamples.map((sample) => {
-        return <Card data={sample} />;
+        return <Card data={sample} key={sample.postID} />;
       })}
+      <FilterButton />
+      <Offcanvas />
     </div>
   );
 }
