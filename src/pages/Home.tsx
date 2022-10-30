8f99f4a6d39c2e53273b9d13f294560d53c89ede
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 19:17:55 +0100

    filter form for desktop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Card from "../components/Card";
 import FilterButton from "../components/FilterButton";
-import Offcanvas from "../components/Offcanvas";
+import FilterForm from "../components/FilterForm";
+import Offcanvas from "../features/offcanvas/Offcanvas";
 
 export default function Home() {
   const cardSamples = [
@@ -18,12 +19,53 @@ export default function Home() {
       content:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
     },
+    {
+      title: "Esto es una prueba 2",
+      userID: 3,
+      postID: 3,
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
+    },
+    {
+      title: "Esto es una prueba 2",
+      userID: 3,
+      postID: 4,
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
+    },
+    {
+      title: "Esto es una prueba 2",
+      userID: 3,
+      postID: 5,
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
+    },
+    {
+      title: "Esto es una prueba 2",
+      userID: 3,
+      postID: 6,
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
+    },
+    {
+      title: "Esto es una prueba 2",
+      userID: 3,
+      postID: 7,
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
+    },
   ];
+
   return (
     <div className="home">
-      {cardSamples.map((sample) => {
-        return <Card data={sample} key={sample.postID} />;
-      })}
+      <div className="home__filter-form">
+        <FilterForm />
+      </div>
+      <div className="home__cards-container">
+        {cardSamples.map((sample) => {
+          return <Card data={sample} key={sample.postID} />;
+        })}
+      </div>
       <FilterButton />
       <Offcanvas />
     </div>
