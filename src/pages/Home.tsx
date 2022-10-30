67c99d4d2a84a094691c12e52decb9540c810e84
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 12:30:24 +0100

    Card Sample

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import Card from "../components/Card";
+
 export default function Home() {
+  const cardSample = {
+    title: "Esto es una prueba",
+    userID: 2,
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
+  };
   return (
-    <>
-      <h1>Home</h1>
-    </>
-  )
+    <div className="home">
+      <Card data={cardSample} />
+    </div>
+  );
 }
