660b9695eec479daba0fb81beef5417f7f10a57e
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 12:33:35 +0100

    array map to generate cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,25 @@
 import Card from "../components/Card";
 
 export default function Home() {
-  const cardSample = {
-    title: "Esto es una prueba",
-    userID: 2,
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
-  };
+  const cardSamples = [
+    {
+      title: "Esto es una prueba",
+      userID: 2,
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
+    },
+    {
+      title: "Esto es una prueba 2",
+      userID: 3,
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque nam adipisci eveniet facere amet ducimus accusamus architecto, ullam repudiandae placeat quasi sequi natus, dolorem rem officia quas perferendis quo libero?",
+    },
+  ];
   return (
     <div className="home">
-      <Card data={cardSample} />
+      {cardSamples.map((sample) => {
+        return <Card data={sample} />;
+      })}
     </div>
   );
 }
