5ef1c7a62f80e91162477f1515c677832e2ea915
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 16:19:44 +0100

    EditPostForm layout

---
+++ src/components/EditPostForm.tsx
@@ -0,0 +1,26 @@
+export default function EditPostForm() {
+  return (
+    <div className="edit-post">
+      <span className="form__span edit-post__span">Post: </span>
+      <span className="form__span edit-post__span">User: </span>
+      <form action="" className="form">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          className="form__input"
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          id="content"
+          cols={30}
+          rows={8}
+          className="form__input form__input--textarea"
+          placeholder="Content"
+        />
+        <button className="button button--offcanvas">edit</button>
+      </form>
+    </div>
+  );
+}
