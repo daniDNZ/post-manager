7dba7ce8327fce9469874b24de5089d5fce7b5c5
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 16:20:04 +0100

    FilterForm layout

---
+++ src/components/FilterForm.tsx
@@ -0,0 +1,31 @@
+export default function FilterForm() {
+  return (
+    <div className="filter-posts">
+      <form action="" className="filter-posts__form form">
+        <div className="form__input-group">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            className="form__input"
+            placeholder="Search..."
+          />
+          <i className="form__icon form__icon--search"></i>
+        </div>
+        <span className="form__span form__span--center">by</span>
+        <select
+          name="selectFilter"
+          id="selectFilter"
+          className="form__input form__input--select"
+        >
+          <option value="title">Title</option>
+          <option value="content">Content</option>
+          <option value="user">User</option>
+          <option value="id">Post ID</option>
+        </select>
+
+        <button className="button button--offcanvas">go</button>
+      </form>
+    </div>
+  );
+}
