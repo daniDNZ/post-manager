532e499f05e21bb5c54dc2502bc3f469499c324f
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 19:30:20 +0100

    Filter button enable offcanvas filter form

--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -1,3 +1,19 @@
+import { useAppDispatch } from "../app/hooks";
+import {
+  changeForm,
+  enableOffcanvas,
+  OffcanvasKind,
+} from "../features/offcanvas/offcanvasSlice";
+
 export default function FilterButton() {
-  return <button className="button button--filter"></button>;
+  const dispatch = useAppDispatch();
+
+  const handleFilterButton = () => {
+    dispatch(changeForm(OffcanvasKind.FILTER_FORM));
+    dispatch(enableOffcanvas());
+  };
+
+  return (
+    <button className="button button--filter" onClick={handleFilterButton} />
+  );
 }
