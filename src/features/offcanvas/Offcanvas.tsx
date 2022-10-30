06c7f44d56cd3060b9b92b49678354b969d715e9
danisanz <daniDNZ@users.noreply.github.com>
2022-10-30 19:19:15 +0100

    Offcanvas moved + redux

---
+++ src/features/offcanvas/Offcanvas.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import EditPostForm from "../../components/EditPostForm";
+import FilterForm from "../../components/FilterForm";
+import {
+  disableOffcanvas,
+  OffcanvasKind,
+  selectOffcanvasStatus,
+  selectOffcanvasType,
+} from "./offcanvasSlice";
+
+export default function Offcanvas() {
+  const dispatch = useAppDispatch();
+  const offcanvasType = useAppSelector(selectOffcanvasType);
+  const offcanvasActive = useAppSelector(selectOffcanvasStatus);
+
+  const formSelector = () => {
+    switch (offcanvasType) {
+      case OffcanvasKind.EDIT_FORM:
+        return <EditPostForm />;
+      case OffcanvasKind.FILTER_FORM:
+      default:
+        return <FilterForm />;
+    }
+  };
+
+  const offcanvasContent = formSelector();
+  const offcanvasDisplay = {
+    display: offcanvasActive ? "flex" : "none",
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+
+    if (body) body.style.overflow = "hidden";
+  }, []);
+  return (
+    <div className="offcanvas" style={offcanvasDisplay}>
+      <button
+        className="offcanvas__close"
+        onClick={() => dispatch(disableOffcanvas())}
+      ></button>
+      <div className="offcanvas__body">{offcanvasContent}</div>
+    </div>
+  );
+}
