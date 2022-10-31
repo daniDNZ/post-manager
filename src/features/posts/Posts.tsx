06e1570c5534d087adb7dfde7980d2cc7c0c6f2d
danisanz <daniDNZ@users.noreply.github.com>
2022-10-31 19:43:35 +0100

    Pagination added

--- src/features/posts/Posts.tsx
+++ src/features/posts/Posts.tsx
@@ -1,21 +1,26 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import Pagination from "./Pagination";
 import Card from "./Post";
-import { fetchPosts, selectPosts } from "./postsSlice";
+import { fetchPosts, selectPosts, selectPostsCurrentPage } from "./postsSlice";
 
 export default function Posts() {
   const posts = useAppSelector(selectPosts);
+  const currentPage = useAppSelector(selectPostsCurrentPage);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchPosts());
-  }, [dispatch]);
+  }, [dispatch, currentPage]);
 
   return (
-    <div className="cards-container">
-      {posts.map((post) => {
-        return <Card data={post} key={post.id} />;
-      })}
-    </div>
+    <>
+      <div className="cards-container">
+        {posts.map((post) => {
+          return <Card data={post} key={post.id} />;
+        })}
+      </div>
+      <Pagination />
+    </>
   );
 }
