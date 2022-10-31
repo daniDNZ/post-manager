eae81d59550dae8eda1c434a3a150a3b39cf00fb
danisanz <daniDNZ@users.noreply.github.com>
2022-10-31 19:42:19 +0100

    new name

---
+++ src/functions/apiFetch.ts
@@ -0,0 +1,33 @@
+import { IPost } from "../features/posts/postsSlice";
+
+export enum FetchMethods {
+  POST = 'POST',
+  GET = 'GET',
+  DELETE = 'DELETE'
+}
+
+type Params = {
+  url: string;
+  method: FetchMethods;
+  body?: IPost;
+}
+
+export default async function apiFetch({ url, method, body = undefined }: Params) {
+  try {
+    const options: RequestInit = {
+      method,
+      body: body ? JSON.stringify(body) : undefined,
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    };
+    const res = await fetch(url, options);
+    const linkHeaders = res.headers.get('Link');
+    const data = await res.json();
+
+    return {data, linkHeaders, success: true};
+  } catch (err) {
+    return {data: {}, linkHeaders: '', success: false};
+  }
+}
