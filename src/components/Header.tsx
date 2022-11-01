e0a5f04a3b5c3fd771fc2786499e70a5db3dd8b2
danisanz <daniDNZ@users.noreply.github.com>
2022-11-01 10:45:31 +0100

    Changed logo url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,12 @@ import Login from "./Login";
 export default function Header() {
   return (
     <header className="header">
-      <img src="./logo.svg" alt="PostManager" loading="lazy"></img>
+      <img
+        src={`${process.env.PUBLIC_URL}/img/svg/logo.svg`}
+        alt="PostManager"
+        loading="lazy"
+        height="30px"
+      ></img>
       <Login />
     </header>
   );
